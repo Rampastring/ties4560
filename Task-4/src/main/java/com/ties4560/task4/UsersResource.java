57c87c73f628c50a38c527ec45c5e1b5e71b249c
Jaro Väisänen <jaro.vaisanen@gmail.com>
2018-10-03 22:37:41 +0300

    User creation, deletion and get. BASIC auth in comments in web.xml.

--- Task-4/src/main/java/com/ties4560/task4/UsersResource.java
+++ Task-4/src/main/java/com/ties4560/task4/UsersResource.java
@@ -3,11 +3,12 @@
  */
 package com.ties4560.task4;
 
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.UUID;
 
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
+import javax.naming.InitialContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -19,11 +20,14 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.apache.catalina.User;
+import org.apache.catalina.UserDatabase;
+
 import beans.Credential;
 
 /**
- * @author Janita
- * @version 30.9.2018
+ * @author Janita, Jaro
+ * @version 3.10.2018
  *
  */
 @Path("/users")
@@ -31,27 +35,33 @@ import beans.Credential;
 @Consumes(MediaType.APPLICATION_JSON)
 @PermitAll
 public class UsersResource {
-	
-	private static HashMap<String, Credential> credentials = new HashMap<String, Credential>();
-	
-	@Path("/authenticate")
-	@POST
-	public Response authenticateUser(String token) {
-		if (credentials.get(token) != null) {
-			Response.status(Status.OK).entity(credentials.get(token)).build();
-		}
-		return Response.status(Status.NOT_FOUND).entity(token).build();
-	}
-	
+
+
+//	@Path("/authenticate")
+//	@POST
+//	public Response authenticateUser(String token) {
+//		if (credentials.get(token) != null) {
+//			Response.status(Status.OK).entity(credentials.get(token)).build();
+//		}
+//		return Response.status(Status.NOT_FOUND).entity(token).build();
+//	}
+
 	@POST
 	public Response createUser(Credential newCredential) {
 		String token = createToken(newCredential);
 		if (token != null && !token.isEmpty()) {
-			credentials.put(token, newCredential);
+//			credentials.put(token, newCredential);
+			try {
+				UserDatabase ud = (UserDatabase) new InitialContext().lookup("java:comp/env/UserDatabase");
+				ud.createUser(newCredential.getName(), newCredential.getPassword(), token);
+				ud.save();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		} else {
 			return Response.status(Status.NOT_ACCEPTABLE).entity(newCredential).build();
 		}
-		
+
 		return Response.status(Status.CREATED).entity(newCredential).build();
 	}
 
@@ -61,31 +71,46 @@ public class UsersResource {
 		}
 		return null;
 	}
-	
+
 	@GET
 	@Path("/{username}")
 	@RolesAllowed("admin")
 	public Response getUser(@PathParam("username") String username) {
-		for (Credential cred : credentials.values()) {
-			if (cred.getName().equals(username)) {
-				return Response.status(Status.OK).entity(cred).build();
+		try {
+			UserDatabase ud = (UserDatabase) new InitialContext().lookup("java:comp/env/UserDatabase");
+			Iterator<User> users = ud.getUsers();
+			User user = null;
+			while (users.hasNext()) {
+				user = (User) users.next();
+				if (user.getUsername().equals(username)) {
+					return Response.status(Status.OK).entity(new Credential(user.getUsername(), user.getPassword())).build();
+				}
 			}
+		} catch (Exception e) {
+			throw new DataNotFoundException("User with username " + username + " was not found.");
 		}
-		throw new DataNotFoundException("User with name "+ username +" was not found.");
+		throw new DataNotFoundException("User with username " + username + " was not found.");
 	}
-	
+
 	@DELETE
 	@Path("/{username}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response deleteUser(@PathParam("username") String username) {
-		for (Credential cred : credentials.values()) {
-			if (cred.getName().equals(username)) {
-				credentials.values().remove(cred);
-				return Response.status(Status.OK).entity(cred).build();
+		try {
+			UserDatabase ud = (UserDatabase) new InitialContext().lookup("java:comp/env/UserDatabase");
+			Iterator<User> users = ud.getUsers();
+			User user = null;
+			while (users.hasNext()) {
+				user = (User) users.next();
+				if (user.getUsername().equals(username)) {
+					ud.removeUser(user);
+					ud.save();
+					return Response.status(Status.OK).entity(new Credential(user.getUsername(), user.getPassword())).build();
+				}
 			}
+		} catch (Exception e) {
+			throw new DataNotFoundException("User with username " + username + " was not found. Cannot delete the user.");
 		}
-		
-		throw new DataNotFoundException("User with name "+ username +" was not found. Cannot delete the user.");
+		throw new DataNotFoundException("User with username " + username + " was not found. Cannot delete the user.");
 	}
-
 }
